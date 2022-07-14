1fcfa839d138dec0dbd6fe38b98a6d4c3b44bd16
chakrit tnasinpaibool <imnew108@gmail.com>
2022-07-14 18:28:04 +0700

    redis

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"time"
-
-	"github.com/gofiber/fiber/v2"
-)
-
 func main () {
-	app:= fiber.New()
-
-	app.Get("/hello",func(c *fiber.Ctx) error {
-		time.Sleep(time.Millisecond * 10)
-		return c.SendString("Hello World")
-	})
-
-	app.Listen(":8000")
+	
 }
